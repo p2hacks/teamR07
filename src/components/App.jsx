1964d7349b6207ad3d011ffe9bc9d0ded3fbe33c
KeitaOsaki <keita.base.05@gmail.com>
2019-12-07 23:18:54 +0900

    雛形の作成

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.css';
+import Header from './Areas/Header';
+import { useDispatch } from 'react-redux';
+
+
+  
+const App = () => {
+  return (
+    <div id="container">
+      <Main/>
+    </div>
+  )
+}
+
+export default App;
