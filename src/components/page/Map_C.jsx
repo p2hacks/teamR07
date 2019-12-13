40d10c115c7479d2b637dfcb3821d18fad1e4c35
take-2405 <take12.wa@icloud.com>
2019-12-14 03:05:35 +0900

    [fix]Map_C

--- src/components/page/Map_C.jsx
+++ src/components/page/Map_C.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MapView from '../elements/MapView';
+import '../../styles/page/Map_C.css';
+
 const Map_C = () => {
     const position = [41.773642, 140.726565]
     const posi = [41.776143, 140.730720]
-   // const posi = [41.776100, 140.730700]
+    // const posi = [41.776100, 140.730700]
     return (
         <div>
             <MapView position={position} posi={posi} />
