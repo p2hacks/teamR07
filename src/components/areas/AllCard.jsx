aecb7ef2c6a02664068b91c8a4a69e83d4fce8a2
KeitaOsaki <keita.base.05@gmail.com>
2019-12-14 04:39:32 +0900

    教会ページのカードの配置

--- src/components/areas/AllCard.jsx
+++ src/components/areas/AllCard.jsx
@@ -4,14 +4,31 @@ import Card_Ka from '../elements/ Card_Church/Card_Ka';
 import Card_Kitoku from '../elements/ Card_Church/Card_Kitoku';
 import Card_Mira from '../elements/ Card_Church/Card_Mira';
 import Card_Pa from '../elements/ Card_Church/Card_Pa';
+import Card_Yo from '../elements/ Card_Church/Card_Yo';
+import '../../styles/areas/AllCard.css';
 const AllCard = () => {
     return (
       <div id='cards'>
+        <div id='cardsContainer'>
+          <div id='HaArea'>
       <Card_Ha/>
+      </div>
+      <div id='KaArea'>
       <Card_Ka/>
+      </div>
+      <div id='KitokuArea'>
       <Card_Kitoku/>
+      </div>
+      <div id='MiraArea'>
       <Card_Mira/>
+      </div>
+      <div id='PaArea'>
       <Card_Pa/>
+      </div>
+      <div id='YoArea'>
+        <Card_Yo/>
+        </div>
+        </div>
       </div>
     )
   }
