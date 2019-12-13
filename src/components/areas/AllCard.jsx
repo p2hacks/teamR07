46e8c0c1b3f0daf0b1760c2f84a5e42011b478c9
KeitaOsaki <keita.base.05@gmail.com>
2019-12-13 22:18:31 +0900

    カードの作成

---
+++ src/components/areas/AllCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card_C from '../elements/Card_C'
+const AllCard = () => {
+    return (
+      <div id='cards'>
+      <Card_C/>
+      </div>
+    )
+  }
+  
+  export default AllCard;
+  
---
+++ src/components/elements/Card_C.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Harisutosu from '../../images/教会画像/ハリストス.jpg';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+const Card_C = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image= {Harisutosu}
+          title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              正ハリストス教会
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            エッチな場所です。クリスマスにここにくるのは童貞かクリボッチくらいでしょう
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            詳細情報
+          </Button>
+          <Button size="small" color="primary">
+            ここへ行く
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
+  export default Card_C;
