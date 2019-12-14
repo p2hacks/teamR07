be283ad466de9ac30bfc6bd9e0d320d5d7298742
take-2405 <take12.wa@icloud.com>
2019-12-14 17:46:47 +0900

    [fix]Home.jsx

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -14,34 +14,29 @@ const Home = () => {
           <h1 id="Top"> 函館おすすめ教会の紹介</h1>
           <h2 id="subtop">まずは函館のクリスマス王道おすすめスポット</h2>
           <p id="attention">注)酔っ払いながら書いているので不適切な表現があるかもしれません</p>
-
-
           <ul>
             <li id="Spot">「函館のクリスマス=クリスマスファンタジー」というほど王道のムーブ</li>
             <p id="Description">クリスマスファンタジーとは??</p>
             <p id="Description">クリスマスファンタジーは、例年北海道函館市で行われるイベント。12月1日からクリスマスである12月25日まで実施される。
           毎年カナダから贈呈される巨大なクリスマスツリーのイルミネーションが著名。</p>
+            <a href="http://www.hakodatexmas.com/">詳しく知りたい方はこちら</a>
             <img id="Picture"
               title="Christmas fantasy"
               src={Kurifan}
               alt="Christmas fantasy"
             />
-            {/*ここにクリファンの公式サイトを貼りたい  */}
-            {/* <script src="http://www.hakodatexmas.com/"></script> */}
-
-
             <li id="Spot">函館駅前のイルミネーションもおすすめ</li>
             <p id="Description">函館駅前の広場がイルミネーションで装飾されます。期間は11/30（土）～2/29（土）です。確実にきれいにみれる時間帯が日没～22:00です。</p>
+            <a href="https://event.hakobura.jp/4421"><br></br>詳しく知りたい方はこちら</a>
             <img id="Picture"
               title="Station Square"
               src={Station}
               alt="Station Square"
             />
-            {/*ここに駅前広場の公式サイトを貼りたい  */}
-
             <li id="Spot">函館山</li>
             <p id="Description">クリスマスだからと言って特にいつもの夜景と変化はありせんがおすすめです。</p>
             <p id="Description">冬に函館の夜景をみたことがないという方には是非おすすめ！！天候に左右されやすいですが、冬の方が夏などよりも空気中のゴミなどがないのでよりきれいに見えるでしょう！</p>
+            <a href="https://334.co.jp/"><br></br>詳しく知りたい方はこちら</a>
             <img id="Picture"
               title="Mount Hakodate"
               src={Yakei}
